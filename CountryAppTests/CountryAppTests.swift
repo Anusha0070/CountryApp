e34dfc99f6911733eb85919b16ed05dd8bfdcc5d
Anusha Raju <anusha.raju0070@gmail.com>
2024-12-12 07:03:17 -0800

    Initial Commit

---
+++ CountryAppTests/CountryAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CountryAppTests.swift
+//  CountryAppTests
+//
+//  Created by Anusha Raju on 12/12/24.
+//
+
+import Testing
+@testable import CountryApp
+
+struct CountryAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
